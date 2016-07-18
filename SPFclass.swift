2619632949d8d6f672ff3675370fe3ca5b52df75
William Possidento <wpossidento@gmail.com>
2016-07-18 13:48:16 -0400

    SPF class in Swift language
    Bridging header option accepted upon creation to link this Swift file to Objective-C files in project.

---
+++ SPFclass.swift
@@ -0,0 +1,21 @@
+//
+//  SPFclass.swift
+//  UV APP
+//
+//  Created by Macbook Pro on 7/18/16.
+//  Copyright © 2016 Clyff IOS supercompany. All rights reserved.
+//
+
+import Foundation
+
+
+class SPF {
+    
+    // "spFactor" from "sun protection factor":
+    var spFactor : Int
+    
+    init(spFactor:Int) {
+        self.spFactor = spFactor
+    }
+    
+}
